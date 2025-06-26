2c38b9f242b29470926c3ab0d2da44c94cab2368
dc-apg <alex.gilbert@defconai.com>
2025-06-26 14:54:13 -0500

    big sync

--- .config/nushell/aliases.nu
+++ .config/nushell/aliases.nu
@@ -5,3 +5,5 @@ alias z = zellij -l welcome
 alias n = nvim
 alias dive = docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive:latest
 alias fp = cd (sh -c "find ~/dev/ -mindepth 3 -maxdepth 4 -type d -not -path '*/.git*' -not -path '*/node_modules/*'" | fzf)
+
+alias dd = ^sh -lc $"nohup dolphin \"($env.PWD)\" > /dev/null 2>&1 &"
--- .config/nvim/lua/plugins/dap_plugins.lua
+++ .config/nvim/lua/plugins/dap_plugins.lua
@@ -1,113 +1,53 @@
-local function dap_menu()
-	local dap = require("dap")
-	local dapui = require("dapui")
-	local dap_widgets = require("dap.ui.widgets")
-
-	local hint = [[
- _t_: Toggle Breakpoint             _R_: Run to Cursor
- _s_: Start                         _E_: Evaluate Input
- _c_: Continue                      _C_: Conditional Breakpoint
- _b_: Step Back                     _U_: Toggle UI
- _d_: Disconnect                    _S_: Scopes
- _e_: Evaluate                      _X_: Close
- _g_: Get Session                   _O_: Step Into
- _y_: Hover Variables               _o_: Step Over
- _r_: Toggle REPL                   _u_: Step Out
- _x_: Terminate                     _p_: Pause
- ^ ^               _q_: Quit
-]]
-
-	return {
-		name = "Debug",
-		hint = hint,
-		config = {
-			color = "pink",
-			invoke_on_body = true,
-			hint = {
-				float_opts = {
-					-- row, col, height, width, relative, and anchor should not be
-					-- overridden
-					style = "minimal",
-					focusable = false,
-					noautocmd = true,
-				},
-				position = "middle-right",
-			},
-		},
-		mode = "n",
-		body = "<A-d>",
-        -- stylua: ignore
-        heads = {
-            { "C", function() dap.set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
-            { "E", function() dapui.eval(vim.fn.input "[Expression] > ") end,        desc = "Evaluate Input", },
-            { "R", function() dap.run_to_cursor() end,                               desc = "Run to Cursor", },
-            { "S", function() dap_widgets.scopes() end,                              desc = "Scopes", },
-            { "U", function() dapui.toggle() end,                                    desc = "Toggle UI", },
-            { "X", function() dap.close() end,                                       desc = "Quit", },
-            { "b", function() dap.step_back() end,                                   desc = "Step Back", },
-            { "c", function() dap.continue() end,                                    desc = "Continue", },
-            { "d", function() dap.disconnect() end,                                  desc = "Disconnect", },
-            {
-                "e",
-                function() dapui.eval() end,
-                mode = { "n", "v" },
-                desc =
-                "Evaluate",
-            },
-            { "g", function() dap.session() end,           desc = "Get Session", },
-            { "y", function() dap_widgets.hover() end,     desc = "Hover Variables", },
-            { "O", function() dap.step_into() end,         desc = "Step Into", },
-            { "o", function() dap.step_over() end,         desc = "Step Over", },
-            { "p", function() dap.pause.toggle() end,      desc = "Pause", },
-            { "r", function() dap.repl.toggle() end,       desc = "Toggle REPL", },
-            { "s", function() dap.continue() end,          desc = "Start", },
-            { "t", function() dap.toggle_breakpoint() end, desc = "Toggle Breakpoint", },
-            { "u", function() dap.step_out() end,          desc = "Step Out", },
-            { "x", function() dap.terminate() end,         desc = "Terminate", },
-            { "q", nil, {
-                exit = true,
-                nowait = true,
-                desc = "Exit"
-            } },
-        },
-	}
-end
-
 local M = {
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
-			{ "rcarriga/nvim-dap-ui" },
+			{ "igorlfs/nvim-dap-view", opts = {} },
 			{ "theHamsta/nvim-dap-virtual-text" },
 			{ "nvim-neotest/nvim-nio" },
 			{ "nvim-telescope/telescope-dap.nvim" },
 			{ "jay-babu/mason-nvim-dap.nvim" },
 			{ "LiadOz/nvim-dap-repl-highlights", opts = {} },
+			{ "nvim-tree/nvim-web-devicons" },
 			{ "echasnovski/mini.icons", opts = {} },
 		},
         -- stylua: ignore
-        keys = {},
+        keys = require("alex-config.keymaps").dap_keys,
 		opts = {},
 		config = function(plugin, opts)
 			require("nvim-dap-virtual-text").setup(opts)
 
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup({
-				icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-				controls = {
-					icons = {
-						pause = "⏸",
-						play = "▶",
-						step_into = "⏎",
-						step_over = "⏭",
-						step_out = "⏮",
-						step_back = "b",
-						run_last = "▶▶",
-						terminate = "⏹",
-						disconnect = "⏏",
-					},
-				},
-			})
+			local dap = require("dap")
+			local dapui = require("dap-view")
+
+			vim.cmd("hi DapBreakpointColor guifg=#e51400") -- Bright red for breakpoints
+			vim.cmd("hi DapBreakpointConditionColor guifg=#ffa500") -- Orange for conditional breakpoints
+			vim.cmd("hi DapBreakpointRejectedColor guifg=#888888") -- Gray for rejected breakpoints
+			vim.cmd("hi DapStoppedColor guifg=#00ff00") -- Green for current execution line
+			vim.cmd("hi DapLogPointColor guifg=#61afef") -- Blue for log points
+
+			-- Define the signs with icons and colors
+			vim.fn.sign_define(
+				"DapBreakpoint",
+				{ text = "●", texthl = "DapBreakpointColor", linehl = "", numhl = "" }
+			)
+
+			vim.fn.sign_define(
+				"DapBreakpointCondition",
+				{ text = "◐", texthl = "DapBreakpointConditionColor", linehl = "", numhl = "" }
+			)
+
+			vim.fn.sign_define(
+				"DapBreakpointRejected",
+				{ text = "●", texthl = "DapBreakpointRejectedColor", linehl = "", numhl = "" }
+			)
+
+			vim.fn.sign_define(
+				"DapStopped",
+				{ text = "󰁕", texthl = "DapStoppedColor", linehl = "DapStoppedLine", numhl = "DapStoppedColor" }
+			)
+
+			vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPointColor", linehl = "", numhl = "" })
 
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
@@ -121,15 +61,7 @@ local M = {
 		end,
 	},
 	{
-		"nvimtools/hydra.nvim",
-		event = "VeryLazy",
-		config = function(_, _)
-			local Hydra = require("hydra")
-			Hydra(dap_menu())
-		end,
-	},
 
-	{
 		"jay-babu/mason-nvim-dap.nvim",
 		dependencies = "mason.nvim",
 		cmd = { "DapInstall", "DapUninstall" },
@@ -143,12 +75,17 @@ local M = {
 	{
 		"nvim-neotest/neotest",
 		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"rouge8/neotest-rust",
 			"nvim-lua/plenary.nvim",
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-treesitter/nvim-treesitter",
+			{
+				"fredrikaverpil/neotest-golang", -- Installation
+				dependencies = {
+					"leoluz/nvim-dap-go",
+				},
+			},
 		},
+		keys = require("alex-config.keymaps").neotest_keys,
 	},
 }
 
