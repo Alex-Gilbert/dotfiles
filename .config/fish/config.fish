0abe28e3881ca725e5d4449d4236cc17c47aaf59
Alex-Gilbert <alexp.gilbert@pm.me>
2025-09-23 10:07:38 -0500

    updating fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,27 +20,38 @@ alias kay kubectl
 alias mouse-speed='xinput set-prop 14 "libinput Accel Speed"'
 alias space-check='sudo du -h --max-depth=1 . | sort -h'
 
-# ATH additions
-set -gx PATH $PATH \
-    $HOME/binaryen-version_123/bin \
-    $HOME/.npm-global/bin \
-    $HOME/rga-2.11.0.28 \
-    $HOME/.local/CPLEX_Studio221/cplex/bin/x86-64_linux \
-    $HOME/.local/zig-0.12.0 \
-    $HOME/.local/pandoc-3.1.8/bin \
-    $HOME/.local/renderdoc_1.31/bin \
-    $HOME/dotfiles/scripts \
-    $HOME/dc-repos/artiv-deployment \
-    $HOME/.local/bin \
-    $HOME/.dotnet/tools \
-    $HOME/.local/azure-functions \
-    $HOME/.cargo/bin \
-    $HOME/dev/defcon/skyshark/target/release \
-    $(go env GOBIN) \
-    $(go env GOPATH)/bin
+# Cross-platform PATH additions
+fish_add_path $HOME/dotfiles/scripts
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
 
-# Environment variables
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+# Check if we're on macOS and source macOS-specific config
+if test (uname -s) = "Darwin"
+    if test -f $HOME/dotfiles/macos/.config/fish/config.fish
+        source $HOME/dotfiles/macos/.config/fish/config.fish
+    end
+else
+    # Linux-specific paths
+    set -gx PATH $PATH \
+        $HOME/binaryen-version_123/bin \
+        $HOME/.npm-global/bin \
+        $HOME/rga-2.11.0.28 \
+        $HOME/.local/CPLEX_Studio221/cplex/bin/x86-64_linux \
+        $HOME/.local/zig-0.12.0 \
+        $HOME/.local/pandoc-3.1.8/bin \
+        $HOME/.local/renderdoc_1.31/bin \
+        $HOME/dc-repos/artiv-deployment \
+        $HOME/.dotnet/tools \
+        $HOME/.local/azure-functions \
+        $HOME/dev/defcon/skyshark/target/release \
+        $(go env GOBIN) \
+        $(go env GOPATH)/bin
+    
+    # Linux-specific environment variables
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+end
+
+# Common environment variables
 set -gx GAMS_VERSION "46.4"
 
 # Zoxide integration
